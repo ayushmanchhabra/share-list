ff38fc582844d3ea4e83793e64de6fbc8b73ac68
Ayushman Chhabra <info@ayushmanchhabra.com>
2023-06-14 00:34:25 +0530

    Save app state in URL

--- index.tsx
+++ index.tsx
@@ -1,5 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
+import {
+  Route,
+  HashRouter as Router,
+  Routes,
+  useNavigate,
+  useParams,
+} from "react-router-dom";
 
 new EventSource("/esbuild").addEventListener("change", () => location.reload());
 
@@ -8,12 +15,51 @@ type Item = {
   focus: boolean;
 };
 
+type AppSchema = {
+  name: Item;
+  items: Item[];
+};
+
 function App() {
   const [name, setName] = useState<Item>({ content: "", focus: true });
   const [items, setItems] = useState<Item[]>([]);
+  const [keyState, setKeyState] = useState<string>("");
+
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (id !== undefined) {
+      const appState: AppSchema = JSON.parse(decompress(atob(id)));
+      setName(appState.name);
+      setItems(appState.items);
+    }
+  }, [id]);
+
+  function compress(data: string): string {
+    return data;
+  }
+
+  function decompress(data: string): string {
+    return data;
+  }
 
   return (
-    <div>
+    <div
+      onKeyDown={(event) => {
+        if (event.key === "Control") {
+          setKeyState(event.key);
+        }
+        if (keyState === "Control" && event.key !== "s") {
+          setKeyState("");
+        }
+        if (keyState === "Control" && event.key === "s") {
+          event.preventDefault();
+          const url = btoa(compress(JSON.stringify({ name, items })));
+          navigate(`/${url}`);
+        }
+      }}
+    >
       <input
         autoFocus={name.focus}
         className={"title"}
@@ -70,5 +116,12 @@ function App() {
 const root: HTMLElement | null = document.getElementById("root");
 
 if (root !== null) {
-  createRoot(root).render(<App />);
+  createRoot(root).render(
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/:id" element={<App />} />
+      </Routes>
+    </Router>
+  );
 }

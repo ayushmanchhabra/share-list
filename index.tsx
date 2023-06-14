2ac350279b681229b777ff64c1b8ee7afa960122
Ayushman Chhabra <info@ayushmanchhabra.com>
2023-06-14 13:13:57 +0530

    Simplify types

--- index.tsx
+++ index.tsx
@@ -8,22 +8,21 @@ import {
   useParams,
 } from "react-router-dom";
 
-// Uncomment during development
-// new EventSource("/esbuild").addEventListener("change", () => location.reload());
+const RELEASE: boolean = false;
+
+if (RELEASE === false) {
+  new EventSource("/esbuild").addEventListener("change", () =>
+    location.reload()
+  );
+}
 
 type Item = {
   content: string;
   focus: boolean;
 };
 
-type AppSchema = {
-  name: Item;
-  items: Item[];
-};
-
 function App() {
-  const [name, setName] = useState<Item>({ content: "", focus: true });
-  const [items, setItems] = useState<Item[]>([]);
+  const [items, setItems] = useState<Item[]>([{ content: "", focus: true }]);
   const [keyState, setKeyState] = useState<string>("");
 
   const { id } = useParams();
@@ -31,9 +30,8 @@ function App() {
 
   useEffect(() => {
     if (id !== undefined) {
-      const appState: AppSchema = JSON.parse(decompress(atob(id)));
-      setName(appState.name);
-      setItems(appState.items);
+      const appState: Item[] = JSON.parse(decompress(atob(id)));
+      setItems(appState);
     }
   }, [id]);
 
@@ -56,59 +54,70 @@ function App() {
         }
         if (keyState === "Control" && event.key === "s") {
           event.preventDefault();
-          const url = btoa(compress(JSON.stringify({ name, items })));
+          const url = btoa(compress(JSON.stringify(items)));
           navigate(`/${url}`);
         }
       }}
     >
-      <input
-        autoFocus={name.focus}
-        className={"title"}
-        onChange={(e) => setName({ content: e.target.value, focus: true })}
-        onKeyDown={(event) => {
-          if (event.key === "Enter" && items.length === 0) {
-            setName({ ...name, content: name.content + " " });
-            setItems([{ content: "", focus: true }]);
-          }
-        }}
-        placeholder={"Untitled"}
-        type="text"
-        value={name.content}
-      />
       {items.map((item: Item, idx: number) => {
-        return (
-          <input
-            autoFocus={item.focus}
-            className={"item"}
-            key={idx}
-            onKeyDown={(event) => {
-              if (event.key === "Enter") {
+        if (idx === 0) {
+          return (
+            <input
+              autoFocus={item.focus}
+              className={"title"}
+              onChange={(event) => {
                 let tempItems = [...items];
-                tempItems[idx].content += " ";
-                setItems([...tempItems, { content: "", focus: true }]);
-              }
-              if (event.key === "Backspace") {
-                let tempItems = [...items];
-                let item = tempItems[idx];
-                if (item.content.length === 0 && idx >= 0) {
-                  event.currentTarget.blur();
-                  const prev = event.currentTarget
-                    .previousSibling as HTMLElement;
-                  prev.focus();
-                  tempItems.splice(idx, 1);
-                  setItems([...tempItems]);
+                tempItems[idx].content = event.target.value;
+                tempItems[idx].focus = true;
+                setItems([...tempItems]);
+              }}
+              onKeyDown={(event) => {
+                if (event.key === "Enter" && items.length === 1) {
+                  let tempItems = [...items];
+                  tempItems[idx].content += " ";
+                  setItems([...tempItems, { content: "", focus: true }]);
                 }
-              }
-            }}
-            onChange={(event) => {
-              let tempItems = [...items];
-              tempItems[idx].content = event.target.value;
-              tempItems[idx].focus = true;
-              setItems([...tempItems]);
-            }}
-            value={item.content}
-          />
-        );
+              }}
+              placeholder={"Untitled"}
+              type="text"
+              value={item.content}
+            />
+          );
+        } else {
+          return (
+            <input
+              autoFocus={item.focus}
+              className={"item"}
+              key={idx}
+              onKeyDown={(event) => {
+                if (event.key === "Enter") {
+                  let tempItems = [...items];
+                  tempItems[idx].content += " ";
+                  setItems([...tempItems, { content: "", focus: true }]);
+                }
+                if (event.key === "Backspace") {
+                  let tempItems = [...items];
+                  let item = tempItems[idx];
+                  if (item.content.length === 0 && idx >= 0) {
+                    event.currentTarget.blur();
+                    const prev = event.currentTarget
+                      .previousSibling as HTMLElement;
+                    prev.focus();
+                    tempItems.splice(idx, 1);
+                    setItems([...tempItems]);
+                  }
+                }
+              }}
+              onChange={(event) => {
+                let tempItems = [...items];
+                tempItems[idx].content = event.target.value;
+                tempItems[idx].focus = true;
+                setItems([...tempItems]);
+              }}
+              value={item.content}
+            />
+          );
+        }
       })}
     </div>
   );

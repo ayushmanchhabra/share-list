ab3dccf8a1c386c55808ed90eb054b936fc9b794
Ayushman Chhabra <info@ayushmanchhabra.com>
2023-07-05 18:28:12 +0530

    build(components): remove List

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,5 +1,4 @@
 export { compress } from "./compress";
-export { dummy } from "./db";
 export { decompress } from "./decompress";
 export { deserialise } from "./deserialise";
 export { serialise } from "./serialise";

5d3618d60922806f20679fcc789108f208aef452
Ayushman Chhabra <ayushmanchhabra@users.noreply.github.com>
2023-08-30 15:17:19 -0400

    test: expect dialog to not be visible if open=false

--- test/UserDialog.spec.tsx
+++ test/UserDialog.spec.tsx
@@ -7,8 +7,14 @@ export default {
   component: UserDialog,
 };
 
-export const UserDialogStory = Object.assign(() => <UserDialog onClose={() => {}} open={true} />, {
+export const UserDialogOpenStory = Object.assign(() => <UserDialog onClose={() => {}} open={true} />, {
   test: async (browser, { component, result }) => {
     await expect(component).to.be.visible;
+  },
+});
+
+export const UserDialogCloseStory = Object.assign(() => <UserDialog onClose={() => {}} open={false} />, {
+  test: async (browser, { component, result }) => {
+    await expect(component).to.not.be.visible;
   },
 });

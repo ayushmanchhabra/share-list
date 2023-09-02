e9da6de2479378ccd18233df8c51894a05cb9efa
Ayushman Chhabra <ayushmanchhabra@users.noreply.github.com>
2023-09-01 20:32:55 -0400

    chore(assets): reference license

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,8 @@
+// Copyright (c) 2023 Simple Icons. CC0 1.0 Universal
+
 import { ReactComponent as SpotifyIcon } from "./spotify.svg"
+
+// Sourced from: https://simpleicons.org/?q=spotify
 
 export {
     SpotifyIcon

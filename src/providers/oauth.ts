5f9a54e73cab2a04ea3510ba2802020f920ef51a
Ayushman Chhabra <ayushmanchhabra@users.noreply.github.com>
2023-09-04 18:55:43 -0400

    chore: remove lingering `console.log` functions

--- src/providers/oauth.ts
+++ src/providers/oauth.ts
@@ -17,16 +17,13 @@ export function useSpotify(clientId: string, redirectUrl: string, scopes: string
         let sdk: SpotifyApi | null = null;
 
         if (accessToken !== null) {
-            console.log("Access Token Strategy");
             sdk = SpotifyApi.withAccessToken(clientId, accessToken as AccessToken, config);
         } else {
-            console.log("PKCE Strategy");
             sdk = SpotifyApi.withUserAuthorization(clientId, redirectUrl, scopes, config);
         }
 
         return sdk.authenticate()
             .then((response) => {
-                console.log(response)
                 if (response.authenticated) {
                     setApi(api);
                     setAccessToken(response.accessToken);

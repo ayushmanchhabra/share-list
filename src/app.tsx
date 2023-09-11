171a8fa822bc332d97269d0f6fdf06b0e3bac5a0
Ayushman Chhabra <ayushmanchhabra@users.noreply.github.com>
2023-09-11 01:23:55 -0400

    feat: create User context

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,20 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
+import { useUser } from "providers";
 import { Home, List } from "routes";
 
 export function App() {
+  const { UserProvider } = useUser();
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/list" element={<List />}>
-          <Route path=":id" element={<List />} />
-        </Route>
-      </Routes>
-    </Router>
+    <UserProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/list" element={<List />}>
+            <Route path=":id" element={<List />} />
+          </Route>
+        </Routes>
+      </Router>
+    </UserProvider>
   );
 }
---
+++ src/providers/contexts/User.tsx
@@ -0,0 +1,22 @@
+import { createContext, useState } from "react";
+
+import { UserSchema } from "providers";
+
+interface UserProviderProps {
+  children: JSX.Element;
+}
+
+export function useUser() {
+  const [user, setUser] = useState<UserSchema | null>(null);
+  const { Provider } = createContext({} as UserSchema);
+
+  function UserProvider({ children }: UserProviderProps) {
+    return <Provider value={{ user }}>{children}</Provider>;
+  }
+
+  return {
+    user,
+    setUser,
+    UserProvider,
+  };
+}

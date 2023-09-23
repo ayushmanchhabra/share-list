32f864fc99d26ed8584d94f2daf1fc31dc614965
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2023-09-23 13:47:14 -0400

    chore: simplify copy

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -17,38 +17,16 @@ export function Home() {
       </Typography>
       <br />
       <Typography variant="h6" className={style.Content}>
-        You really like X streaming service.
+        {"Search and import songs and playlists."}
+        <br />
+        {"Create your ideal playlist."}
+        <br />
+        {"Share it with your friends and family."}
+        <br />
+        {"They'll export the playlist to their own streaming service."}
+        <br />
+        {"Or they'll tweak it and send it to others."}
       </Typography>
-      <Typography variant="h6" className={style.Content}>
-        But your family is on Y streaming service and your friends are on Z
-        streaming service.
-      </Typography>
-      <Typography variant="h6" className={style.Content}>
-        Sharing your music becomes unnecessarily hard.
-      </Typography>
-      <br />
-      <Typography variant="h6" className={style.Content}>
-        Well, not anymore:
-      </Typography>
-
-      <Typography variant="h6" className={style.Content}>
-        - Import existing playlists via X streaming service
-      </Typography>
-      <Typography variant="h6" className={style.Content}>
-        - Send the uniquely generated link to your family.
-      </Typography>
-      <Typography variant="h6" className={style.Content}>
-        - Your family will open the link, may edit and export the playlist to Y
-        streaming service.
-      </Typography>
-      <Typography variant="h6" className={style.Content}>
-        - Send the uniquely generated link to your friend.
-      </Typography>
-      <Typography variant="h6" className={style.Content}>
-        - Your friend will open the link, may edit and export the playlist to Z
-        streaming service.
-      </Typography>
-      <br />
       <Typography variant="h6" className={style.Content}>
         Get Started...
         <IconButton

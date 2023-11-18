ec20660f7b551978763de143564e48fa5b3c7e9d
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2023-11-18 14:16:11 -0500

    fix: lint remaining codebase

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -3,26 +3,17 @@ import { Box, Typography } from "@mui/material";
 import style from "./Home.module.scss";
 
 export function Home() {
-
   return (
     <Box className={style.Home}>
       <Typography variant="h2" className={style.Header} data-testid="title">
         {"Share List"}
       </Typography>
       <br />
-      <Typography
-        variant="h4"
-        className={style.Content}
-        data-testid="subtitle"
-      >
+      <Typography variant="h4" className={style.Content} data-testid="subtitle">
         {"Share and remix music playlists across platforms."}
       </Typography>
       <br />
-      <Typography
-        variant="h6"
-        className={style.Content}
-        data-testid="content"
-      >
+      <Typography variant="h6" className={style.Content} data-testid="content">
         {"Coming soon..."}
       </Typography>
       <br />

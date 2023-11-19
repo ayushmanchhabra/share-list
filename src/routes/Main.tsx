cf5ac8a4f8925449f276a8e050f08f02341e4614
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2023-11-19 15:21:21 -0500

    feat: add search bar

--- src/routes/Main.tsx
+++ src/routes/Main.tsx
@@ -1,12 +1,44 @@
-import { AppBar, Box, Typography } from "@mui/material";
+import { Search } from "@mui/icons-material";
+import { AppBar, Box, TextField, Typography } from "@mui/material";
 
-export function Main () {
-    return (
-        <Box>
-            <AppBar>
-                <Typography variant="h4">Share List</Typography>
-            </AppBar>
-          
-        </Box>
-    );
+export function Main() {
+  return (
+    <Box>
+      <AppBar
+        position="static"
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+
+          padding: 1,
+        }}
+      >
+        <Typography
+          variant="h6"
+          fontWeight={"600"}
+          sx={{
+            width: "200px",
+            minWidth: "140px",
+            marginLeft: "10px",
+          }}
+        >
+          SHARE LIST
+        </Typography>
+        <Search />
+        <TextField
+          autoFocus
+          fullWidth
+          inputProps={{
+            sx: {
+              color: "white",
+              marginLeft: "10px",
+            },
+          }}
+          placeholder="Search..."
+          variant="standard"
+        />
+      </AppBar>
+    </Box>
+  );
 }
